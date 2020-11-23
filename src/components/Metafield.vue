eaa2be21be6cfbc986011ff8a6a4c0b7ac8c25f9
Chris Everson <chris@chriseverson.net>
2020-11-23 19:49:51 +1100

    feat: add metafield component

---
+++ src/components/Metafield.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="metafield">
+    <div class="metafield__desc">
+      <label v-if="label" v-html="label"></label>
+      <p v-if="description" v-html="description"></p>
+    </div>
+    <div class="metafield__body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    props: {
+      label: {
+        type: String,
+        default: null,
+      },
+      description: {
+        type: String,
+        default: null,
+      }
+    },
+  }
+</script>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Metafield from './components/Metafield.vue';
+
+export default {
+  install(Vue, options) {
+    Vue.component('metafield', Metafield);
+  }
+}
