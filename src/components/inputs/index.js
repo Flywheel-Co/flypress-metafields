ea1586491eb6357ace6b28ee263eeede33d295ac
Chris Everson <chris@chriseverson.net>
2020-11-24 13:32:41 +1100

    feat: add number input component

--- src/components/inputs/index.js
+++ src/components/inputs/index.js
@@ -2,10 +2,12 @@ import TextInput from './TextInput.vue';
 import UrlInput from './UrlInput.vue';
 import TelInput from './TelInput.vue';
 import EmailInput from './EmailInput.vue';
+import NumberInput from './NumberInput.vue';
 
 export default {
   TextInput,
   UrlInput,
   TelInput,
   EmailInput,
+  NumberInput,
 }
