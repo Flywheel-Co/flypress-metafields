ed349cf4d9323456e48c979e902d224e1a1b071e
Chris Everson <chris@chriseverson.net>
2020-11-24 13:01:51 +1100

    feat: add tel input component

--- src/components/inputs/index.js
+++ src/components/inputs/index.js
@@ -1,7 +1,9 @@
 import TextInput from './TextInput.vue';
 import UrlInput from './UrlInput.vue';
+import TelInput from './TelInput.vue';
 
 export default {
   TextInput,
   UrlInput,
+  TelInput,
 }
