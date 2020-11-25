0baff239d70c0b14ad8a374184a72fd37a33a0c6
Chris Everson <chris@chriseverson.net>
2020-11-25 12:35:51 +1100

    feat: add spinner component

---
+++ src/components/utils/Spinner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="metainput__spinner">
+    <div class="spinner is-active"></div>
+    <span v-if="label">{{ label }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      label: {
+        type: String,
+        default: null
+      }
+    }
+  }
+</script>
